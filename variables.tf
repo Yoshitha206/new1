4d56c46592405761e3541278b66a45ca090f83af
Yoshitha206 <yoshithaa35@gmail.com>
2024-08-02 14:19:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subscription1_id" {
+  description = "The ID of Subscription 1"
+}
+
+variable "subscription1_client_id" {
+  description = "The Client ID for Subscription 1"
+}
+
+variable "subscription1_client_secret" {
+  description = "The Client Secret for Subscription 1"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID"
+}
